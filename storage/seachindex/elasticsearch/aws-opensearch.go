cbc8541728617b67d83c2e45c10f9d555a3db2f0
harphies <taofeekhorlarlaykan@gmail.com>
2023-10-06 15:27:02 +0100

    postgre TBD

--- storage/seachindex/elasticsearch/aws-opensearch.go
+++ storage/seachindex/elasticsearch/aws-opensearch.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/opensearch-project/opensearch-go/v2"
 	"github.com/opensearch-project/opensearch-go/v2/opensearchapi"
 	requestsigner "github.com/opensearch-project/opensearch-go/v2/signer/awsv2"
 	"go.uber.org/zap"
