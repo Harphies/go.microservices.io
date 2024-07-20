adbb4c4c539bcb0350c335008e4b8bc0779e359c
harphies <taofeekhorlarlaykan@gmail.com>
2024-07-20 09:46:58 +0100

    tidy

--- security/iam/authorization/authorize.go
+++ security/iam/authorization/authorize.go
@@ -2,3 +2,4 @@ package authorization
 
 // https://github.com/hashicorp-demoapp/product-api-go/blob/main/handlers/auth.go#L54
 // https://github.com/chartmuseum/auth/blob/main/authorization.go
+//https://www.ardanlabs.com/blog/2024/07/go-package-design-authorization-and-api-structures-ep-9.html
