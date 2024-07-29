afbb9abc0a3f54be01e7aa99cdb1c3ed67f5e3b1
harphies <taofeekhorlarlaykan@gmail.com>
2024-07-29 01:08:00 +0100

    added

--- utils/http-helpers.go
+++ utils/http-helpers.go
@@ -341,3 +341,12 @@ func ExtractBearerToken(r *http.Request) (string, error) {
 	}
 	return parts[1], nil
 }
+
+// addCORSHeaders sets the CORS headers for the response
+func addCORSHeaders(w http.ResponseWriter, origin string) {
+	w.Header().Set("Access-Control-Allow-Origin", origin)
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Origin")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Max-Age", "86400")
+}
